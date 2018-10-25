bfd00475bf3bbf27e1a5e4b222258a7bac1fed9d
gitoutplayed <mfan991@gmail.com>
2018-10-25 15:21:21 -0400

    Updated documentation

--- LevelManager.java
+++ LevelManager.java
@@ -5,65 +5,104 @@ import java.util.List;
  * This class represents the LevelManager.
  * 
  * @author Michael Fan 101029934
+ * @version Oct 25, 2018
  */
 
 public class LevelManager {
-    private List<Level> levels;
-    private int currentLevel;
+	private List<Level> levels;
+	private int currentLevel;
 
-    public LevelManager() {
-	levels = new LinkedList<Level>();
-	currentLevel = -1;
-	createLevels();
-    }
-
-    public void addLevel(Level level) {
-	levels.add(level);
-    }
-
-    public Level getNextLevel() {
-	if (currentLevel == levels.size()) {
-	    return null;
+	/**
+	 * Creates a new LevelManager.
+	 */
+	public LevelManager() {
+		levels = new LinkedList<Level>();
+		currentLevel = -1;
+		createLevels();
 	}
 
-	return levels.get(++currentLevel);
-    }
-
-    public Level getPreviousLevel() {
-	if (currentLevel == 0) {
-	    return null;
+	/**
+	 * Adds a new level.
+	 * 
+	 * @param level the level to be added
+	 */
+	public void addLevel(Level level) {
+		levels.add(level);
 	}
 
-	return levels.get(--currentLevel);
-    }
+	/**
+	 * Returns the next level.
+	 * 
+	 * @return the next level
+	 */
+	public Level getNextLevel() {
+		if(currentLevel == levels.size()) {
+			return null;
+		}
 
-    public Level getLevel(int index) {
-	if (index < 0 || index >= levels.size()) {
-	    return null;
+		return levels.get(++currentLevel);
 	}
 
-	currentLevel = index;
-	return levels.get(index);
-    }
+	/**
+	 * Returns the previous level.
+	 * 
+	 * @return the previous level
+	 */
+	public Level getPreviousLevel() {
+		if(currentLevel == 0) {
+			return null;
+		}
 
-    public int size() {
-	return levels.size();
-    }
+		return levels.get(--currentLevel);
+	}
 
-    public int getCurrentLevel() {
-	return currentLevel;
-    }
+	/**
+	 * Returns the level at the specified index.
+	 * 
+	 * @param index the index of the level
+	 * 
+	 * @return the level at the specified index
+	 */
+	public Level getLevel(int index) {
+		if(index < 0 || index >= levels.size()) {
+			return null;
+		}
 
-    public void createLevels() {
-	// Create the levels here
-	Level level = new Level(1);
-	level.addPlant(PlantName.PeaShooter);
-	level.addPlant(PlantName.SunFlower);
-	level.addZombie(ZombieTypes.WALKER, 10);
-	level.setBaseSunCounterGain(25);
-	level.setSpawnRate(2);
-	level.setSpawnAmount(1);
+		currentLevel = index;
+		return levels.get(index);
+	}
 
-	levels.add(level);
-    }
+	/**
+	 * Returns the total number of levels.
+	 * 
+	 * @return the total number of levels
+	 */
+	public int size() {
+		return levels.size();
+	}
+
+	/**
+	 * Returns the index of the current level.
+	 * 
+	 * @return the index of the current level
+	 */
+	public int getCurrentLevel() {
+		return currentLevel;
+	}
+	
+	/**
+	 * Creates and appends new levels.
+	 */
+	public void createLevels() {
+		// Create the levels here
+		Level level = new Level(1);
+		level.addPlant(PlantName.PeaShooter);
+		level.addPlant(PlantName.SunFlower);
+		level.addZombie(ZombieTypes.WALKER, 10);
+		level.setBaseSunGain(25);
+		level.setSpawnRate(2);
+		level.setSpawnAmount(1);
+
+		levels.add(level);
+	}
 }
--- Shop.java
+++ Shop.java
@@ -7,43 +7,65 @@ import java.util.Set;
  * the level.
  *
  * @author Michael Fan 101029934
+ * @version Oct 25, 2018
  */
 
 public class Shop {
-    private Map<PlantName, Integer> shop;
-
-    public Shop() {
-	shop = new HashMap<PlantName, Integer>();
-    }
-
-    public void addPlants(Set<PlantName> shop) {
-	for (PlantName p : shop) {
-	    Plant plant = PlantFactory.createPlant(p);
-	    this.shop.putIfAbsent(p, plant.getPrice());
-	}
-    }
-
-    public String getShopItems() {
-	StringBuilder sb = new StringBuilder();
+	private Map<PlantName, Integer> shop;
 	
-	for(PlantName p : shop.keySet()) {
-	    sb.append(p.name() + " " + shop.get(p) + '\n');
+	/**
+	 * Creates a new shop.
+	 */
+	public Shop() {
+		shop = new HashMap<PlantName, Integer>();
 	}
 	
-	return sb.toString();
-    }
-
-    public Plant purchase(PlantName plant, int sunCounter) {
-	if (!shop.containsKey(plant)) {
-	    return null;
+	/**
+	 * Adds plants that are available purchase into the shop.
+	 * 
+	 * @param plants the plants that are to be added to the shop
+	 */
+	public void addPlants(Set<PlantName> plants) {
+		for(PlantName p : plants) {
+			Plant plant = PlantFactory.createPlant(p);
+			this.shop.putIfAbsent(p, plant.getPrice());
+		}
 	}
+	
+	/**
+	 * Returns the plants and their prices as a string.
+	 * 
+	 * @return the plants and their prices
+	 */
+	public String getShopPlants() {
+		StringBuilder sb = new StringBuilder();
 
-	Plant newPlant = PlantFactory.createPlant(plant);
+		for(PlantName p : shop.keySet()) {
+			sb.append(p.name() + " " + shop.get(p) + '\n');
+		}
 
-	if (sunCounter < shop.get(plant)) {
-	    return null;
+		return sb.toString();
 	}
+	
+	/**
+	 * Purchase a plant.
+	 * 
+	 * @param plant the plant to be purchased
+	 * @param sunCounterthe current amount of sun counter
+	 * 
+	 * @return the purchased plant if purchased successfully or false otherwise
+	 */
+	public Plant purchase(PlantName plant, int sunCounter) {
+		if(!shop.containsKey(plant)) {
+			return null;
+		}
 
-	return newPlant;
-    }
+		Plant newPlant = PlantFactory.createPlant(plant);
+
+		if(sunCounter < shop.get(plant)) {
+			return null;
+		}
+
+		return newPlant;
+	}
 }
