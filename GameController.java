61c4baac4eb00cbb2242375a0fc47e101f12a37a
gitoutplayed <mfan991@gmail.com>
2018-10-27 15:02:03 -0400

    minor change

--- GameController.java
+++ GameController.java
@@ -6,8 +6,6 @@ import java.io.InputStreamReader;
  * This class represents the GameController.
  * 
  * @author Tamer Ibrahim 101032919
- * @editor Hoang Bui 101029049
- * @version 2
  */
 public class GameController {
 
@@ -30,6 +28,10 @@ public class GameController {
 			BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
 
 			while(true) {
+				if(game.isLevelFinished() ) {
+					break;
+				}
+				
 				System.out.println("\n\n#####################################\n\n\nThe current grid is:");
 				gameView.printGrid();
 				// Printing out more details about the game
@@ -75,10 +77,6 @@ public class GameController {
 				else {
 					System.out.println("Invalid command");
 				}
-				
-				if(game.isLevelFinished() ) {
-					break;
-				}
 			}
 		}
 
@@ -94,10 +92,10 @@ public class GameController {
 	 *
 	 */
 	public void printHelp() {
-		System.out.println("\n1. write 'buy plant' to buy plants");
-		System.out.println("2. write 'shovel' to shovel a plant");
-		System.out.println("3. write 'end turn' to end a turn");
-		System.out.println("4. write 'quit' to quite the game \n");
+		System.out.println("\n1. write buy plant to buy plants");
+		System.out.println("2. write shovel to shovel a plant");
+		System.out.println("3. write end turn to end a turn");
+		System.out.println("4. write quit to quite the game \n");
 	}
 
 	/**
@@ -142,9 +140,9 @@ public class GameController {
 
 			PlantName name;
 			// checking the type that the player has entered
-			if(type.equalsIgnoreCase("peashooter")) {
+			if(type.equalsIgnoreCase("pea shooter")) {
 				name = PlantName.PeaShooter;
-			} else if(type.equalsIgnoreCase("sunflower")) {
+			} else if(type.equalsIgnoreCase("sun flower")) {
 				name = PlantName.SunFlower;
 			} else {
 				return false;
