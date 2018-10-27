c6aeb650296eb275f578f8b3e7f103e21d9d0c3f
TamerIbrahim97 <tameribrahim89@gmail.com>
2018-10-27 02:18:37 -0400

    Update GameView.java

--- GameView.java
+++ GameView.java
@@ -2,41 +2,66 @@ import java.util.List;
 
 import javax.swing.JButton;
 
+/**
+ * This class represents the GameView.
+ * 
+ * @author Tamer Ibrahim 101032919
+ */
 public class GameView {
 
-	//	private  JButton grid [][];
-
-	private Game game; 
+	private Game game;
 
 	public GameView(Game game) {
 		this.game = game;
 	}
+	
+	/**
+	 * prints the current turn number
+	 */
+	public void printCurrentTurn() {
+		int currentTurn= game.getTurnNumber();
+		System.out.println(currentTurn);
+	}
+	/**
+	 * prints the sun counter.
+	 * 
+	 */
+	
+	public void prtinSunCounter() {
+		int sunCounter = game.getSunCounter();
+		System.out.println(sunCounter);
+	}
 
+	/**
+	 * prints the types of zombies.
+	 * 
+	 */
+	public void printZombie() {
+		String zombieType = game.getAvailableZombies();
+		System.out.println(zombieType);
+	}
 
-
-	//	public String printZombie(){
-	//		
-	//	}
-
-	//	public void printPlants(){
-	//	}
-
-
-	public void printGrid(){
-		//loop through the state and print line by line
+	/**
+	 * prints the grid.
+	 * 
+	 */
+	public void printGrid() {
+		// loop through the state and print line by line
 		String[][] readOnly = game.getGrid();
-		for(int i = 0; i < readOnly.length; i++) {
-			for(int j = 0; j < readOnly[i].length; j++) {
+		for (int i = 0; i < readOnly.length; i++) {
+			for (int j = 0; j < readOnly[i].length; j++) {
 				System.out.print(readOnly[i][j]);
-			}	
+			}
 			System.out.println();
 		}
 	}
 
-	public void printShop(){
-		//		System.out.println(currentPlants );
-		//		System.out.println(sunCounter);
+	/**
+	 * Prints the plants in the shop and their prices.
+	 * 
+	 */
+	public void printShop() {
+		String Printshop = game.getShopItems();
+		System.out.println(Printshop);
 	}
 }
-
-
