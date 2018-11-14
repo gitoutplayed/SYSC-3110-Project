1942d82fae7c818ef0e2f94d7278f6f580099a24
Michael Fan <mfan991@gmail.com>
2018-11-14 08:52:40 -0500

    Attempt to recover the repo

---
+++ software project/src/zombie/Walker.java
@@ -0,0 +1,12 @@
+package zombie;
+/**
+ * Class for walker zombie
+ * 
+ * @author Hoang Bui 1010129049
+ * @version 2, October 16 2018
+ */
+public class Walker extends Zombie{
+	public Walker() {
+		super(3, 10, 30, 1, ZombieTypes.WALKER);
+	}
+}
---
+++ software project/src/zombie/ZombieTypes.java
@@ -0,0 +1,10 @@
+package zombie;
+/**
+ * Enum file to hold all zombie types
+ * @author Hoang Bui 101029049
+ * @version 1
+ */
+
+public enum ZombieTypes {
+	WALKER;
+}
