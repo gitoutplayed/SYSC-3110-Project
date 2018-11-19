ad490cddeedaffd21ec6bba1ea1e5155370dea9f
Hoang Bui <hoangxuanbui6572@gmail.com>
2018-11-19 18:08:56 -0500

    Update new levels

--- software project/src/game/LevelManager.java
+++ software project/src/game/LevelManager.java
@@ -1,4 +1,5 @@
 package game;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -10,6 +11,7 @@ import zombie.ZombieTypes;
  * loading. 
  * 
  * @author Michael Fan 101029934
+ * @editor Hoang Bui 101029049
  * @version Oct 25, 2018
  */
 
@@ -99,15 +101,71 @@ public class LevelManager {
 	 * Creates and appends new levels.
 	 */
 	public void createLevels() {
-		// Create the levels here
-		Level level = new Level(1);
-		level.addPlant(PlantName.PeaShooter);
-		level.addPlant(PlantName.SunFlower);
-		level.addZombie(ZombieTypes.WALKER, 10);
-		level.setBaseSunGain(25);
-		level.setSpawnRate(2);
-		level.setSpawnAmount(1);
+		//Create variables
+		int levelCounter = 1;
+		ArrayList<PlantName> plants = new ArrayList<PlantName>();
+		ArrayList<ZombieTypes> zombies = new ArrayList<ZombieTypes>();
+		ArrayList<Integer> numZombies = new ArrayList<Integer>();
+		
+		//Add values into lists
+		plants.add(PlantName.PeaShooter);
+		plants.add(PlantName.SunFlower);
+		zombies.add(ZombieTypes.WALKER);
+		numZombies.add(10);
+		
+		//Create level 1
+		createLevelsHelper(levelCounter, plants, zombies, numZombies, 25, 2, 1);
+		
+		//Create level 2
+		levelCounter++;
+		zombies.add(ZombieTypes.CONEHAT);
+		numZombies.set(0, 25);
+		numZombies.add(20);
+		
+		createLevelsHelper(levelCounter, plants, zombies, numZombies, 25, 4, 3);
+		
+		//Create level 3
+		levelCounter++;
+		zombies.add(ZombieTypes.BUCKETHAT);
+		numZombies.set(0, 30);
+		numZombies.set(1, 25);
+		numZombies.add(20);
+		
+		createLevelsHelper(levelCounter, plants, zombies, numZombies, 25, 5, 3);
+		
+//		Level level = new Level(1);
+//		level.addPlant(PlantName.PeaShooter);
+//		level.addPlant(PlantName.SunFlower);
+//		level.addZombie(ZombieTypes.WALKER, 10);
+//		level.setBaseSunGain(25);
+//		level.setSpawnRate(2);
+//		level.setSpawnAmount(1);
 
+//		levels.add(level);
+	}
+	
+	/**
+	 * 
+	 */
+	private void createLevelsHelper(int levelCounter, ArrayList<PlantName> plants, ArrayList<ZombieTypes> zombies, ArrayList<Integer> zombieNum, int baseSunGain, int spawnRate, int spawnAmount) {
+		//Create new level
+		Level level = new Level(levelCounter);
+		
+		//add plants
+		for(PlantName plant : plants) {
+			level.addPlant(plant);
+		}	
+		
+		//add zombies
+		for(int i = 0; i < zombies.size(); i++) {
+			level.addZombie(zombies.get(i), zombieNum.get(i));
+		}
+		
+		//add spawn and generation values
+		level.setBaseSunGain(baseSunGain);
+		level.setSpawnRate(spawnRate);
+		level.setSpawnAmount(spawnAmount);
+		
 		levels.add(level);
 	}
 }
